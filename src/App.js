f97bb2963d9773084044c4c791e6459fa5d23701
tegarkatresna23 <tegarkatresna@gmail.com>
2024-11-29 11:15:02 +0700

    tegar

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Header from './components/Header'
+import Home from './components/Home'
+import SideNav from './components/SideNav'
+import Footer from './components/Footer'
+
+
+
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Home/>
+    <SideNav/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
